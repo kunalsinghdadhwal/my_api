9a2916c1b0f06d6e5a4650380604f78b131f7538
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-04-07 20:11:56 +0530

    Logger and preety logger setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
+import { logger } from "./middlewares/pino-logger.js";
+import type { PinoLogger } from "hono-pino";
 
-const app = new OpenAPIHono();
+
+interface AppBindings {
+  Variables: {
+    logger: PinoLogger;
+  }
+}
+
+const app = new OpenAPIHono<AppBindings>();
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/error", (c) => {
+  c.status(422);
+  c.var.logger.info("This is an info log");
+  throw new Error("Oops! Something went wrong.");
+})
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
