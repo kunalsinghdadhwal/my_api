ac8819167011892650390a74e99f7d406dd345eb
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-12-14 15:35:55 +0530

    Env
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { serve } from "@hono/node-server";
+
+import app from "./app";
+import { parseEnv } from "./env";
+
+// eslint-disable-next-line node/no-process-env
+const env = parseEnv(process.env);
+
+const port = env.PORT;
+
+console.log(`🔥 Server is running on http://localhost:${port}`);
+console.log(`📚 API Documentation: http://localhost:${port}/reference`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
